55ffe182d4760439c6984bd5878135e362158770
Klava <kristinasun55@gmail.com>
2024-12-03 20:46:55 +0300

    New

--- src/Weather.java
+++ src/Weather.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import org.json.JSONArray; // добавлена библиотека json.org version 20231013
+import org.json.JSONObject;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Weather {
+    public static void main(String[] args) throws Exception {
+        String accessKey = "4f9f0819-0c41-436f-acd5-87b4126d77e6";
+        HttpURLConnection connection = (HttpURLConnection) new URL("https://api.weather.yandex.ru/v2/forecast?lat=52.37125&lon=4.89388").openConnection();
+        connection.setRequestProperty("X-Yandex-Weather-Key", accessKey);
+        Scanner scanner = new Scanner(connection.getInputStream());
+        String json = scanner.useDelimiter("\\A").next();
+        scanner.close();
+
+        JSONObject jsonObject = new JSONObject(json);
+
+        // Получение текущей температуры
+        double currentTemperature = jsonObject.getJSONObject("fact").getDouble("temp");
+        System.out.println("Текущая температура: " + currentTemperature + "°C");
+
+        // Получаем массив forecasts и hours
+        JSONArray forecasts = jsonObject.getJSONArray("forecasts");
+        JSONArray hoursForecast = forecasts.getJSONObject(0).getJSONArray("hours");
+
+        // Переменные для суммирования температуры за текущие сутки
+        double totalTemperature = 0.0;
+        int count = 0;
+
+        // Перебор массива для получения суммы температур за сутки
+        for (int i = 0; i < hoursForecast.length(); i++) {
+            JSONObject hourData = hoursForecast.getJSONObject(i);
+            double temperatureHour = hourData.getDouble("temp");
+            totalTemperature += temperatureHour;
+            count++;
+        }
+
+        // Вычисление среднего значения температуры за сутки
+        if (count > 0) {
+            double dailyAverageTemperature = totalTemperature / count;
+            System.out.printf("Среднее значение температуры за текущие сутки: %.2f°C%n", dailyAverageTemperature);
         }
     }
 }
